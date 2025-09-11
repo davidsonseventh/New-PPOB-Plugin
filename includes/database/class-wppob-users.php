5c0086b2ff95fa51b8a33a5cd418cdb566dabf58
Davidson I Rumondor <61097972+davidsonseventh@users.noreply.github.com>
2025-09-11 10:57:14 +0800

    Create class-wppob-users.php

---
+++ includes/database/class-wppob-users.php
@@ -0,0 +1,28 @@
+<?php
+defined('ABSPATH') || exit;
+
+class WPPOB_Users {
+
+    public static function search_users($search_term) {
+        $users = get_users([
+            'search' => '*' . esc_attr($search_term) . '*',
+            'search_columns' => ['user_login', 'user_email'],
+            'number' => 10
+        ]);
+        return $users;
+    }
+
+    public static function get_user_details($user_id) {
+        $user = get_user_by('id', $user_id);
+        if (!$user) {
+            return null;
+        }
+
+        return [
+            'id' => $user->ID,
+            'username' => $user->user_login,
+            'email' => $user->user_email,
+            'balance' => wppob_format_rp(WPPOB_Balances::get_user_balance($user->ID)),
+        ];
+    }
+}
