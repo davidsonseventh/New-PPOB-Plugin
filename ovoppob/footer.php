6d197b9dac732fd20410d1c4c255dfa14890c15d
Davidson I Rumondor <61097972+davidsonseventh@users.noreply.github.com>
2025-09-16 17:04:30 +0800

    Create footer.php

---
+++ ovoppob/footer.php
@@ -0,0 +1,6 @@
+</div><footer class="site-footer">
+        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. Didukung oleh WP PPOB Manager.</p>
+    </footer>
+    <?php wp_footer(); ?>
+</body>
+</html>
