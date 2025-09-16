660c6ecb1277055f561fb8f1807fd7866a5b8bab
Davidson I Rumondor <61097972+davidsonseventh@users.noreply.github.com>
2025-09-17 01:43:36 +0800

    Delete ovoppob directory

--- ovoppob/footer.php
+++
@@ -1,6 +0,0 @@
-</div><footer class="site-footer">
-        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. Didukung oleh WP PPOB Manager.</p>
-    </footer>
-    <?php wp_footer(); ?>
-</body>
-</html>
