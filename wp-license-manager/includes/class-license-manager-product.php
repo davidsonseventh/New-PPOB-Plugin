8b15db2fd8cd725e107274132b5500de24dd6bec
Davidson I Rumondor <61097972+davidsonseventh@users.noreply.github.com>
2025-09-17 15:28:40 +0800

    Update class-license-manager-product.php

--- wp-license-manager/includes/class-license-manager-product.php
+++ wp-license-manager/includes/class-license-manager-product.php
@@ -6,6 +6,38 @@ class WPLM_Product {
         add_action('woocommerce_order_status_completed', [$this, 'generate_license_on_purchase']);
     }
 
+
+
+// Di dalam class WPLM_Product
+
+// Tambahkan hook baru di __construct()
+public function __construct() {
+    add_action('woocommerce_order_status_completed', [$this, 'generate_license_on_purchase']);
+    
+    // Hook untuk menambah field di halaman edit produk
+    add_action('woocommerce_product_options_general_product_data', [$this, 'add_download_link_field']);
+    add_action('woocommerce_process_product_meta', [$this, 'save_download_link_field']);
+}
+
+// Tambahkan fungsi baru ini
+public function add_download_link_field() {
+    woocommerce_wp_text_input([
+        'id'          => '_wplm_download_link',
+        'label'       => __('Plugin Download Link', 'wp-license-manager'),
+        'placeholder' => 'https://example.com/path/to/plugin.zip',
+        'desc_tip'    => 'true',
+        'description' => __('Enter the direct download link for this licensed product.', 'wp-license-manager'),
+    ]);
+}
+
+// Tambahkan fungsi baru ini
+public function save_download_link_field($post_id) {
+    $download_link = isset($_POST['_wplm_download_link']) ? esc_url_raw($_POST['_wplm_download_link']) : '';
+    update_post_meta($post_id, '_wplm_download_link', $download_link);
+}
+    
+
+    
     /**
      * Fungsi yang dipicu saat pesanan WooCommerce selesai.
      *
