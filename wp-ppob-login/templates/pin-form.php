96352124cd93b72591c81aa8342c9f27f7bedb00
Davidson I Rumondor <61097972+davidsonseventh@users.noreply.github.com>
2025-09-20 22:26:06 +0800

    Create pin-form.php

---
+++ wp-ppob-login/templates/pin-form.php
@@ -0,0 +1,14 @@
+<div id="wppob-pin-form-container" style="display: none;">
+    <h2>Buat PIN Keamanan</h2>
+    <form id="wppob-pin-form">
+        <div class="form-group">
+            <label for="pin">Buat PIN Keamanan</label>
+            <input type="password" id="pin" name="pin" maxlength="6" required>
+        </div>
+        <div class="form-group">
+            <label for="confirm_pin">Konfirmasi PIN Keamanan</label>
+            <input type="password" id="confirm_pin" name="confirm_pin" maxlength="6" required>
+        </div>
+        <button type="submit" class="button">Daftar</button>
+    </form>
+</div>
