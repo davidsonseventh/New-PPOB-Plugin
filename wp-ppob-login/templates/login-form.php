649a6405bd08272743ce8b9d1f7af737c029720f
Davidson I Rumondor <61097972+davidsonseventh@users.noreply.github.com>
2025-09-20 22:23:42 +0800

    Create login-form.php

---
+++ wp-ppob-login/templates/login-form.php
@@ -0,0 +1,14 @@
+<div id="wppob-login-form-container">
+    <h2>Transaksi Lebih Mudah</h2>
+    <p>Belanja Kebutuhan Kamu Dimana Saja dan Kapan Saja</p>
+    <form id="wppob-login-form">
+        <div class="form-group">
+            <label for="phone">Masukkan Nomor HP Kamu untuk Lanjut</label>
+            <div class="input-group">
+                <span>+62</span>
+                <input type="tel" id="phone" name="phone" placeholder="812-3456-7890" required>
+            </div>
+        </div>
+        <button type="submit" class="button">Lanjut</button>
+    </form>
+</div>
